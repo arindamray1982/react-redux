d4d8b6568c32cccc90342b74521a0a2113257edd
arindamray1982 <arin.arindam.ray@gmail.com>
2020-09-01 22:53:26 -0500

    First react app

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div>
+      <h1>This is the App component</h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
